efefd7e2ee9cad963295be60862485fc5ddc866f
ibrahimatix0x01 <96072328+ibrahimatix0x01@users.noreply.github.com>
2022-12-30 08:36:35 -0500

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Enter a domain name:"
-read domain
+read -p "Enter a domain name: " domain
+
 echo "[*] Launching Amass..."
 # Use amass to gather subdomains
 amass enum -passive -norecursive -noalts -d $domain >> subdomains.txt
+
 echo "[*] Launching Subfinder..."
 # Use subfinder to gather subdomains
 subfinder -d $domain >> subdomains.txt
@@ -14,5 +15,33 @@ sort subdomains.txt | uniq > subdomains_deduped.txt
 
 # Rename deduped file to original name
 mv subdomains_deduped.txt subdomains.txt
+
+SUBRES=$(cat subdomains.txt | wc -l)
+echo -e "\n[+] Amass and Subfinder found ${SUBRES} subdomains"
+
 echo "[*] Launching httpx..."
 cat subdomains.txt | httpx -o live-subdomains.txt
+
+LIVESUB=$(cat subdomains.txt | wc -l)
+echo -e "\n[+] httpx found ${LIVESUB} live subdomains"
+
+echo "[*] Launching Nuclei..."
+
+nuclei -list live-subdomains.txt -o nuclei_result.txt
+
+echo "[*] Launching gau & waybackurls to gather urls..."
+
+# Use gau to gather URLs for the specified domain
+gau $domain > urls.txt
+
+# Use waybackurls to gather additional URLs for the specified domain
+waybackurls $domain >> urls.txt
+
+# Sort and remove duplicates from the URLs list
+sort -u urls.txt -o urls.txt
+
+echo "URLs for $domain have been saved to urls.txt"
+echo "[*] Launching kxss for xss scans..."
+cat urls.txt | kxss > xss.txt
+XSSRES=$(cat xss.txt | wc -l)
+echo -e "\n[+] kxss found ${XSSRES} potential xss"
